b281da8df78e7e0f006b77d88e71c6c262df62f0
Phil Lopreiato <plnyyanks@gmail.com>
2014-02-19 14:47:38 -0500

    removed unnecessary fragments

--- frcvolhelper/src/main/java/com/plnyyanks/frcvolhelper/activities/EventDownloadActivity.java
+++ frcvolhelper/src/main/java/com/plnyyanks/frcvolhelper/activities/EventDownloadActivity.java
@@ -51,20 +51,4 @@ public class EventDownloadActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_event_download, container, false);
-            return rootView;
-        }
-    }
-
 }
