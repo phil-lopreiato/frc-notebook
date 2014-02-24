5f203e47594dea55f1168a0adfb0089af3b92ad7
Phil Lopreiato <plnyyanks@gmail.com>
2014-02-23 23:55:37 -0500

    UI update, move of processing to background thread for View match activity

--- frc-notebook/build/source/r/debug/com/plnyyanks/frcnotebook/R.java
+++ frc-notebook/build/source/r/debug/com/plnyyanks/frcnotebook/R.java
@@ -33,12 +33,14 @@ public final class R {
         public static final int ic_launcher=0x7f020006;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f0a0013;
-        public static final int action_delete=0x7f0a001f;
-        public static final int action_settings=0x7f0a0020;
+        public static final int LinearLayout1=0x7f0a0017;
+        public static final int action_delete=0x7f0a0024;
+        public static final int action_settings=0x7f0a0025;
+        public static final int add_note_button=0x7f0a0013;
         public static final int alliance_container=0x7f0a0009;
-        public static final int blue_allaince=0x7f0a000d;
-        public static final int blue_header=0x7f0a000c;
+        public static final int blue_allaince=0x7f0a000f;
+        public static final int blue_header=0x7f0a000d;
+        public static final int blue_score=0x7f0a000e;
         public static final int button=0x7f0a0004;
         public static final int container=0x7f0a0007;
         public static final int download_event_header=0x7f0a0000;
@@ -46,26 +48,29 @@ public final class R {
         public static final int event_list_to_download=0x7f0a0002;
         public static final int event_loading_progress=0x7f0a0001;
         public static final int event_view=0x7f0a0006;
-        public static final int general_notes=0x7f0a0016;
-        public static final int match_list=0x7f0a0014;
-        public static final int match_notes=0x7f0a0019;
+        public static final int general_notes=0x7f0a001b;
+        public static final int match_list=0x7f0a0019;
+        public static final int match_notes=0x7f0a001e;
         public static final int match_title=0x7f0a0008;
-        public static final int matchlist_group=0x7f0a001d;
-        public static final int matchlist_item=0x7f0a001e;
-        public static final int new_general_note=0x7f0a0017;
-        public static final int next_match=0x7f0a0011;
-        public static final int prev_match=0x7f0a0010;
-        public static final int red_alliance=0x7f0a000b;
+        public static final int matches_loading_progress=0x7f0a0018;
+        public static final int matchlist_group=0x7f0a0022;
+        public static final int matchlist_item=0x7f0a0023;
+        public static final int new_general_note=0x7f0a001c;
+        public static final int next_match=0x7f0a0015;
+        public static final int prev_match=0x7f0a0014;
+        public static final int red_alliance=0x7f0a000c;
         public static final int red_header=0x7f0a000a;
+        public static final int red_score=0x7f0a000b;
         public static final int select_event_header=0x7f0a0003;
-        public static final int submit_general_note=0x7f0a0018;
-        public static final int team_list=0x7f0a001c;
-        public static final int team_list_header=0x7f0a001a;
-        public static final int team_notes=0x7f0a000e;
-        public static final int team_notes_list=0x7f0a000f;
-        public static final int team_view=0x7f0a0012;
-        public static final int teams_loading_progress=0x7f0a001b;
-        public static final int view_event_scroll=0x7f0a0015;
+        public static final int submit_general_note=0x7f0a001d;
+        public static final int team_list=0x7f0a0021;
+        public static final int team_list_header=0x7f0a001f;
+        public static final int team_note_container=0x7f0a0011;
+        public static final int team_notes=0x7f0a0010;
+        public static final int team_notes_list=0x7f0a0012;
+        public static final int team_view=0x7f0a0016;
+        public static final int teams_loading_progress=0x7f0a0020;
+        public static final int view_event_scroll=0x7f0a001a;
     }
     public static final class layout {
         public static final int activity_event_download=0x7f030000;
@@ -92,35 +97,46 @@ public final class R {
     public static final class string {
         public static final int action_delete=0x7f070000;
         public static final int action_settings=0x7f070001;
-        public static final int add_general_note_button=0x7f070002;
+        public static final int add_note_button=0x7f070002;
         public static final int app_name=0x7f070003;
-        public static final int download_event_button=0x7f070004;
-        public static final int download_event_title=0x7f070005;
-        public static final int elim_match_header=0x7f070006;
-        public static final int event_content_layout=0x7f070007;
-        public static final int hello_world=0x7f070008;
-        public static final int new_general_note=0x7f070009;
-        public static final int pref_header_appearance=0x7f07000a;
-        public static final int pref_header_general=0x7f07000b;
-        public static final int pref_title_data_source=0x7f07000c;
-        public static final int pref_title_theme=0x7f07000d;
-        public static final int pref_title_year=0x7f07000e;
-        public static final int qual_match_header=0x7f07000f;
-        public static final int select_event_title=0x7f070010;
-        public static final int teams_attending_header=0x7f070011;
-        public static final int title_activity_event_download=0x7f070012;
-        public static final int title_activity_settings=0x7f070013;
-        public static final int title_activity_start=0x7f070014;
-        public static final int title_activity_view_event=0x7f070015;
-        public static final int title_activity_view_match=0x7f070016;
-        public static final int title_activity_view_team=0x7f070017;
+        public static final int blue_alliance=0x7f070004;
+        public static final int blue_score=0x7f070005;
+        public static final int download_event_button=0x7f070006;
+        public static final int download_event_title=0x7f070007;
+        public static final int elim_match_header=0x7f070008;
+        public static final int event_content_layout=0x7f070009;
+        public static final int hello_world=0x7f07000a;
+        public static final int match_title_placeholder=0x7f07000b;
+        public static final int new_general_note=0x7f07000c;
+        public static final int next_match_button=0x7f07000d;
+        public static final int no_team_selected=0x7f07000e;
+        public static final int no_teams=0x7f07000f;
+        public static final int pref_header_appearance=0x7f070010;
+        public static final int pref_header_general=0x7f070011;
+        public static final int pref_title_data_source=0x7f070012;
+        public static final int pref_title_theme=0x7f070013;
+        public static final int pref_title_year=0x7f070014;
+        public static final int prevous_match_button=0x7f070015;
+        public static final int qual_match_header=0x7f070016;
+        public static final int red_alliance=0x7f070017;
+        public static final int red_score=0x7f070018;
+        public static final int select_event_title=0x7f070019;
+        public static final int team_notes_header=0x7f07001a;
+        public static final int teams_attending_header=0x7f07001b;
+        public static final int title_activity_event_download=0x7f07001c;
+        public static final int title_activity_settings=0x7f07001d;
+        public static final int title_activity_start=0x7f07001e;
+        public static final int title_activity_view_event=0x7f07001f;
+        public static final int title_activity_view_match=0x7f070020;
+        public static final int title_activity_view_team=0x7f070021;
     }
     public static final class style {
         public static final int activity_header=0x7f080000;
-        public static final int theme_dark=0x7f080001;
+        public static final int text_super_large=0x7f080001;
+        public static final int theme_dark=0x7f080002;
         /**  Customize your theme here. 
          */
-        public static final int theme_light=0x7f080002;
+        public static final int theme_light=0x7f080003;
     }
     public static final class xml {
         public static final int pref_appearance=0x7f040000;
---
+++ frc-notebook/src/main/java/com/plnyyanks/frcnotebook/background/GetMatchInfo.java
@@ -0,0 +1,195 @@
+package com.plnyyanks.frcnotebook.background;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.plnyyanks.frcnotebook.Constants;
+import com.plnyyanks.frcnotebook.R;
+import com.plnyyanks.frcnotebook.activities.StartActivity;
+import com.plnyyanks.frcnotebook.datatypes.Match;
+import com.plnyyanks.frcnotebook.datatypes.Note;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * Created by phil on 2/23/14.
+ */
+public class GetMatchInfo extends AsyncTask<String,String,String> {
+
+
+    private Activity activity;
+    String    previousMatchKey,
+              thisMatchKey,
+              nextMatchKey,
+              eventKey;
+
+    public GetMatchInfo(Activity activity) {
+        this.activity = activity;
+    }
+    @Override
+    protected String doInBackground(String... strings) {
+        previousMatchKey = strings[0];
+        thisMatchKey     = strings[1];
+        nextMatchKey     = strings[2];
+        eventKey         = strings[3];
+
+        Match match = StartActivity.db.getMatch(thisMatchKey);
+        TextView matchTitle = (TextView) activity.findViewById(R.id.match_title);
+        String titleString = match.getMatchType()+(match.getMatchType().equals("Quals")?" ":(" "+match.getSetNumber()+ " Match ")+match.getMatchNumber());
+        matchTitle.setText(titleString);
+
+
+        TextView redHeader = (TextView)activity.findViewById(R.id.red_score);
+        redHeader.setText(Integer.toString(match.getRedScore())+ " Points");
+
+        TextView blueHeader = (TextView)activity.findViewById(R.id.blue_score);
+        blueHeader.setText(Integer.toString(match.getBlueScore())+" Points");
+
+        LinearLayout redList = (LinearLayout) activity.findViewById(R.id.red_alliance);
+        LinearLayout blueList = (LinearLayout) activity.findViewById(R.id.blue_allaince);
+
+        JsonArray redTeams  = match.getRedAllianceTeams(),
+                blueTeams = match.getBlueAllianceTeams();
+
+        if(redTeams.size() >0){
+            redList.removeAllViews();
+            Iterator<JsonElement> iterator = redTeams.iterator();
+            JsonElement team;
+            while(iterator.hasNext()){
+                team = iterator.next();
+                redList.addView(makeTextView(team.getAsString(), Constants.lparams));
+            }
+        }
+        if(blueTeams.size() >0){
+            blueList.removeAllViews();
+            Iterator<JsonElement> iterator = blueTeams.iterator();
+            JsonElement team;
+            while(iterator.hasNext()){
+                team = iterator.next();
+                blueList.addView(makeTextView(team.getAsString(),Constants.lparams));
+            }
+        }
+
+        if(!StartActivity.db.matchExists(nextMatchKey)){
+            Button nextButton = (Button)activity.findViewById(R.id.next_match);
+            nextButton.setVisibility(View.GONE);
+        }
+        if(!StartActivity.db.matchExists(previousMatchKey)){
+            Button prevButton = (Button)activity.findViewById(R.id.prev_match);
+            prevButton.setVisibility(View.GONE);
+        }
+        return null;
+    }
+
+    private TextView makeTextView(String teamKey,LinearLayout.LayoutParams lparams){
+        TextView tv;
+        tv = new TextView(activity);
+        tv.setLayoutParams(Constants.lparams);
+        tv.setTextSize(25);
+        tv.setText(teamKey.substring(3));
+        tv.setTag(teamKey);
+        tv.setOnClickListener(new TeamClickListener(teamKey,eventKey));
+        ArrayList<Note> notes = StartActivity.db.getAllNotes(teamKey,eventKey);
+        if(notes.size() >0){
+            tv.setTypeface(null, Typeface.BOLD);
+        }
+        return tv;
+    }
+
+    class TeamClickListener implements View.OnClickListener{
+
+        String teamKey,eventKey;
+
+        public TeamClickListener(String teamKey, String eventKey){
+            this.teamKey = teamKey;
+            this.eventKey = eventKey;
+        }
+
+        @Override
+        public void onClick(View view) {
+
+            TextView noteHeader = (TextView)activity.findViewById(R.id.team_notes);
+            noteHeader.setText("Team "+teamKey.substring(3)+" Notes");
+            fetchNotes();
+
+        }
+
+        private void fetchNotes(){
+            ArrayList<Note> notes = StartActivity.db.getAllNotes(teamKey,eventKey);
+            LinearLayout notesList = (LinearLayout)activity.findViewById(R.id.team_notes_list);
+            notesList.removeAllViews();
+            if(notes.size() == 0){
+                TextView t = new TextView(activity);
+                t.setLayoutParams(Constants.lparams);
+                t.setText("No Notes for This Team");
+                notesList.addView(t);
+            }else{
+                for(Note n:notes){
+                    TextView t = new TextView(activity);
+                    t.setLayoutParams(Constants.lparams);
+                    t.setText("• " + n.getNote());
+                    t.setTextSize(18);
+                    notesList.addView(t);
+                }
+            }
+            Button addNote = (Button)activity.findViewById(R.id.add_note_button);
+            addNote.setVisibility(View.VISIBLE);
+            addNote.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    final EditText noteEditField = new EditText(activity);
+                    noteEditField.setText("");
+                    noteEditField.setHint("Enter your note");
+                    AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                    builder.setTitle("Note on Team " + teamKey.substring(3));
+                    builder.setView(noteEditField);
+                    builder.setPositiveButton("Add",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    String resultText;
+                                    Note note = new Note(eventKey,thisMatchKey,teamKey,noteEditField.getText().toString());
+                                    if(StartActivity.db.addNote(note) != -1){
+                                        resultText = "Note added sucessfully";
+                                        fetchNotes();
+                                    }else{
+                                        resultText = "Error adding note to database";
+                                    }
+                                    Toast.makeText(activity, resultText, Toast.LENGTH_SHORT).show();
+                                    dialog.cancel();
+                                }
+                            });
+
+                    builder.setNegativeButton("Cancel",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+                    builder.create().show();
+
+                }
+            });
+        }
+
+        private void addView(View view){
+            LinearLayout notesList = (LinearLayout)activity.findViewById(R.id.team_notes_list);
+            notesList.addView(view);
+        }
+    }
+}
