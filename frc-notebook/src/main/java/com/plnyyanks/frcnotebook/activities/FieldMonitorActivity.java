a356e5ce24dbc1e1af35c453f92cd86f6e4d2d7e
Phil Lopreiato <plnyyanks@gmail.com>
2014-03-14 20:39:04 -0400

    added activity for field monitor webview

---
+++ frc-notebook/src/main/java/com/plnyyanks/frcnotebook/activities/FieldMonitorActivity.java
@@ -0,0 +1,55 @@
+package com.plnyyanks.frcnotebook.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+
+import com.plnyyanks.frcnotebook.R;
+
+public class FieldMonitorActivity extends Activity {
+
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_field_monitor);
+
+        webview = (WebView)findViewById(R.id.field_monitor_webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("http://10.0.100.5/FieldMonitor");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.field_monitor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch(id){
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_refresh_field_monitor:
+                if(webview!=null){
+                    webview.reload();
+                }
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
